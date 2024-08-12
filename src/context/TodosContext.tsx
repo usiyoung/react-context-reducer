c6a22649c02939e1055de2671c1355d375c9dffb
usiyoung <usiyoung7@gmail.com>
2024-08-12 23:56:08 +0900

    feat: Add Todos Context

---
+++ src/context/TodosContext.tsx
@@ -0,0 +1,26 @@
+import { createContext, useReducer, ReactNode, Dispatch, useEffect, useContext } from 'react';
+import { TodosReducer } from '../reducer/TodosReducer.ts';
+import { Todo, TodosAction } from '../types/Todo.ts';
+import { useLocalStorage } from '../hooks/useLocalStorage.ts';
+
+const STORAGE_KEY = 'todos';
+
+interface TodosContextProps {
+    todos: Todo[];
+    dispatch: Dispatch<TodosAction>;
+}
+
+const TodosContext = createContext<TodosContextProps>({ todos: [], dispatch: () => null });
+
+export function TodosProvider({ children }: { children: ReactNode }) {
+    const [storedValue, setLocalStorage] = useLocalStorage(STORAGE_KEY, []);
+    const [todos, dispatch] = useReducer(TodosReducer, storedValue);
+
+    useEffect(() => {
+        setLocalStorage(todos as []);
+    }, [todos]);
+
+    return <TodosContext.Provider value={{ todos: storedValue, dispatch }}>{children}</TodosContext.Provider>;
+}
+
+export const useTodos = () => useContext(TodosContext);
